e68e745b8aa063657b34b92f0761063816eaea6d
parasite381 <77892257+parasite381@users.noreply.github.com>
2026-02-10 14:16:23 +0800

    Change server port from 5100 to 8081

---
+++ joshbackendserver.py
@@ -0,0 +1,35 @@
+from flask import Flask, Response
+from flask_socketio import SocketIO
+
+eventlet.monkey_patch()
+app = Flask(__name__)
+
+last_value=None
+tempstore={}
+socketio=SocketIO(app,cors_allowed_origins="*",async_mode="eventlet")
+@app.route('/')
+@socketio.on('connect')
+def handle_connect():
+  #user connect place 
+  print("a user has connected")
+@socketio.on('disconnect')
+def handle_disconnect():
+    #user disconnected
+    print("a user has disconnected")
+@socketio.on("message")
+def message(msg):
+    print(msg)
+@socketio.on("new_value")
+def newval(dat):
+    print(dat)
+    socketio.emit("new_value", {"value":dat})
+    tempstore['num']=dat
+
+@socketio.on("getres")
+def getres():
+   storeddata=tempstore.get('num',None)
+   socketio.emit("resval",{"value":storeddata})
+   print("From Pricchatmonth GET")
+
+if __name__ == "__main__":
+    socketio.run(app,host="0.0.0.0", port=8081)
