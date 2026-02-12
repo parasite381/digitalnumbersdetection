9cf82ab9c5381c914d4424afa85cd73510fa2bc7
parasite381 <77892257+parasite381@users.noreply.github.com>
2026-02-13 07:38:03 +0800

    Update joshbackendserver.py

--- joshbackendserver.py
+++ joshbackendserver.py
@@ -9,24 +9,24 @@ tempstore={}
 socketio=SocketIO(app,cors_allowed_origins="*",async_mode="gevent")
 @app.route('/')
 @socketio.on('connect')
-   def handle_connect():
+def handle_connect():
    #user connect place 
    print("a user has connected")
 @socketio.on('disconnect')
-    def handle_disconnect():
+def handle_disconnect():
     #user disconnected
     print("a user has disconnected")
 @socketio.on("message")
-    def message(msg):
+def message(msg):
     print(msg)
 @socketio.on("new_value")
-    def newval(dat):
+def newval(dat):
     print(dat)
     socketio.emit("new_value", {"value":dat})
     tempstore['num']=dat
 
 @socketio.on("getres")
-    def getres():
+def getres():
     storeddata=tempstore.get('num',None)
     socketio.emit("resval",{"value":storeddata})
     print("From Pricchatmonth GET")
