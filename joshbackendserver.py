ff88e6e79d6129d1516765349e498177f0a4436f
parasite381 <77892257+parasite381@users.noreply.github.com>
2026-02-13 07:28:56 +0800

    Update joshbackendserver.py

--- joshbackendserver.py
+++ joshbackendserver.py
@@ -1,5 +1,4 @@
 
-
 from flask import Flask, Response
 from flask_socketio import SocketIO
 
@@ -10,27 +9,27 @@ tempstore={}
 socketio=SocketIO(app,cors_allowed_origins="*",async_mode="gevent")
 @app.route('/')
 @socketio.on('connect')
-def handle_connect():
-  #user connect place 
-  print("a user has connected")
+   def handle_connect():
+   #user connect place 
+   print("a user has connected")
 @socketio.on('disconnect')
-def handle_disconnect():
+    def handle_disconnect():
     #user disconnected
     print("a user has disconnected")
 @socketio.on("message")
-def message(msg):
+    def message(msg):
     print(msg)
 @socketio.on("new_value")
-def newval(dat):
+    def newval(dat):
     print(dat)
     socketio.emit("new_value", {"value":dat})
     tempstore['num']=dat
 
 @socketio.on("getres")
-def getres():
-   storeddata=tempstore.get('num',None)
-   socketio.emit("resval",{"value":storeddata})
-   print("From Pricchatmonth GET")
+    def getres():
+    storeddata=tempstore.get('num',None)
+    socketio.emit("resval",{"value":storeddata})
+    print("From Pricchatmonth GET")
 
 if __name__ == "__main__":
     from gevent import pywsgi
